b0a15d5808f7ff1d1e70bb5f8643910f616fbdb2
Z_Ding <dinzonnyuoe@gmail.com>
2023-12-04 14:32:31 +0100

    a

--- AdventOfCode/Source.cpp
+++ AdventOfCode/Source.cpp
@@ -3,58 +3,79 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <set>
+#include <unordered_set>
+#include <unordered_map>
 using namespace std;
 int main() {
     string line;
-    vector<string> matrix;
-    ifstream file("day3part2.txt");
+    ifstream file("day4part1.txt");//card number, multiplier
     int res = 0;
+    unordered_map<int, int> mp;
+    int card = 0;
+    for (int i = 1; i < 1000000; i++) mp[i] = 1;
     if (file.is_open()) {
         while (getline(file, line)) {
-            matrix.push_back(line);
+            card++;
+            int idx = line.find(":");
+            int partition = line.find("|");
+            idx += 2;
+            string num;
+            unordered_set<int> u;
+            while (idx < partition) {
+                if (line[idx] == ' ') {
+                    if (!num.size()) {
+                        idx++;
+                        continue;
+                    }
+                    u.emplace(stoi(num));
+                    num = "";
+                    idx++;
+                    continue;
+                }
+                while (isdigit(line[idx])) {
+                    num += line[idx];
+                    idx++;//idx stops at a space
+                }
+            }
+            //idx at '|'
+            //cout << idx << endl;
+            num = "";
+            idx = partition +2;
+            int match = 0;
+            while (idx < line.size()) {
+                if (line[idx] == ' ' ) {
+                    //cout << "num " << num << endl;
+                    if ( !num.size() || !isdigit(num[0])) {
+                        num = "";          
+                    }
+                    else {
+                        if (u.find(stoi(num)) != u.end()) match++;
+                        //cout << "stoi" << endl;
+                        num = "";
+                    }                    
+                    idx++;
+                    continue;
+                }
+                while ( isdigit(line[idx])) {
+                    num += line[idx];
+                    idx++;//idx stops at a space
+                }
+            }
+            if (num.size()) {
+                if (u.find(stoi(num)) != u.end()) match++;
+            }
+            //create copy for card number [card+1,card+match]
+            int curcopy = mp[card];
+            for (int j = card + 1; j <= card + match; j++) {            
+                mp[j] += curcopy ;
+            }
         }
         file.close();
     }
-    int m = matrix.size();
-    int n = matrix[0].size();
-    // 8 directions
-    vector<int> dx = { 0,0, 1,1,1, -1,-1 ,-1};
-    vector<int> dy = { 1,-1,0,1,-1, 0,1,-1 };  
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (matrix[i][j] == '*') {
-                set<pair<int, int>> visited;
-                vector<int> v;
-                int number = 0;
-                for (int k = 0; k < 8; k++) {
-                    int x = i + dx[k];
-                    int y = j + dy[k];
-                    if (x < 0 || y < 0 || x == m || y == n || !isdigit(matrix[x][y]) || visited.find({x,y}) != visited.end()) continue;
-                    //a number found. search greedy to left and right and mark all continuous number as visited
-                    number++;
-                    if (number > 2) break;
-                    visited.insert({ x,y });
-                    string left;
-                    string right;
-                    int l = y - 1;
-                    int r = y + 1;
-                    while (l >= 0 && isdigit(matrix[x][l])) {
-                        left.insert(left.begin(), matrix[x][l]);
-                        visited.insert({ x,l });
-                        l--;
-                    }
-                    while (r < n && isdigit(matrix[x][r])) {
-                        right += matrix[x][r];
-                        visited.insert({ x,r });
-                        r++;
-                    }
-                    string cur = left + matrix[x][y] + right;
-                    v.push_back(stoi(cur));
-                }
-                if (v.size() == 2) res += v[0] * v[1];
-            }
-        }
+    for (auto& e : mp) {      
+        if (e.first <= card) {
+            res += e.second;
+        } 
     }
     cout << res << endl;
     return 0;
